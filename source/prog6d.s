fad3c12f94c44f67892abd6346c9e162c4fa9c3e
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-03-22 18:09:00 +0000

    Create prog6d.s

---
+++ source/prog6d.s
@@ -0,0 +1,10 @@
+/* multiply two numbers R0=R1*R2 and add one */
+  .global _start
+_start:
+  MOV R1, #20           @ R1=20
+  MOV R2, #5            @ R2=5
+  MOV R3, #10           @ R3=10
+  MLA R0, R1, R2, R3    @ R0=(R1*R2)+R3
+  
+  MOV R7, #1            @ exit through syscall
+  SWI 0
