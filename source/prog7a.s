0494d6a19d951dff9cff4bbcd0da4003b9813ee8
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-03-22 18:16:43 +0000

    Create prog7a.s

---
+++ source/prog7a.s
@@ -0,0 +1,16 @@
+/* How to use Syscall 4 to write a string */
+  .global _start
+_start:
+  MOV R7, #4        @ Syscall number
+  MOV R0, #1        @ Stdout is monitor
+  MOV R2, #19       @ string is 19 chars long
+  LDR R1,=string    @ string located at string:
+  SWI 0
+
+_exit:
+  MOV R7, #1    @ exit syscall
+  SWI 0
+
+.data
+string:
+.ascii "Hello World String\n"
