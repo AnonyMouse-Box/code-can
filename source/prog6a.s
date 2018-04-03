630046c9aed93952e6769b04f6d7784c16468c0a
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-03 13:20:41 +0100

    Update prog6a.s

--- source/prog6a.s
+++ source/prog6a.s
@@ -3,7 +3,7 @@
 _start:
   MOV R1, #50       @ Get 50 into R1
   MOV R2, #60       @ Get 60 into R2
-  ADDS R0, R1, R2   @ Add the two, result in R0
+  ADD S R0, R1, R2   @ Add the two, result in R0
   
   MOV R7, #1        @ exit through syscall
   SWI 0
