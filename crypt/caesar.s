bf79cd170af7ad28483c38612ecc3a3fc995650e
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-04 13:06:26 +0100

    Update caesar.s

--- crypt/caesar.s
+++ crypt/caesar.s
@@ -2,15 +2,26 @@
   .global _start
 _start:
 _read:
+                    @ read syscall
   MOV R7, #3        @ Syscall number
   MOV R0, #0        @ Stdin is keyboard
-  MOV R2, #1        @ read first 1 characters
+  MOV R2, #5        @ read first 5 characters
   LDR R1,=string    @ string placed at string:
   SWI 0
 
 _write:
-  MOV R0, #1
-  MOV R2, #1
-  MOV R7, #4
+                    @ write syscall
+  MOV R7, #4        @ Syscall number
+  MOV R0, #1        @ Stdout is monitor
+  MOV R2, #19       @ string is 19 chars long
+  LDR R1,=string    @ string is located at string:
   SWI 0
-  MOV PC, LR
+
+_exit:
+  @ exit syscall
+  MOV R7, #1
+  SWI 0
+
+.data
+string:
+.ascii "Hello World String\n"
