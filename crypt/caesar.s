62a5c261a9d244317c6eec67c228504862da4f86
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-04 13:05:10 +0100

    Update caesar.s

--- crypt/caesar.s
+++ crypt/caesar.s
@@ -1,10 +1,16 @@
 /* Program for performing a Caesar Cipher to a string */
-.global _start
+  .global _start
 _start:
 _read:
-                    @ read syscall
   MOV R7, #3        @ Syscall number
   MOV R0, #0        @ Stdin is keyboard
-  MOV R2, #5        @ read first 5 characters
+  MOV R2, #1        @ read first 1 characters
   LDR R1,=string    @ string placed at string:
   SWI 0
+
+_write:
+  MOV R0, #1
+  MOV R2, #1
+  MOV R7, #4
+  SWI 0
+  MOV PC, LR
