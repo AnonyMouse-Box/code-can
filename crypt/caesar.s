8718c73243f827320752e8a0f22d0366a33499af
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-04 12:52:29 +0100

    Create caesar.s

---
+++ crypt/caesar.s
@@ -0,0 +1,9 @@
+  .global _start
+_start:
+_read:
+                    @ read syscall
+  MOV R7, #3        @ Syscall number
+  MOV R0, #0        @ Stdin is keyboard
+  MOV R2, #5        @ read first 5 characters
+  LDR R1,=string    @ string placed at string:
+  SWI 0
