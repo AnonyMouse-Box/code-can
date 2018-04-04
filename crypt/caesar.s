cac2d4c2a534d0843c752a6d0ba45241d4b2c321
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-04 18:09:38 +0100

    Update caesar.s

--- crypt/caesar.s
+++ crypt/caesar.s
@@ -5,8 +5,20 @@ _read:
   MOV R7, #3        @ Syscall number
   MOV R0, #0        @ Stdin is keyboard
   MOV R2, #1        @ read first character
+/* needs to be: 40<x<5B or 60<x<7B */
   LDR R1,=string    @ string placed at string:
   SWI 0
+
+_lower:
+  LDR R1,=string      @ address of char
+  LDR R0, [R1]        @ load it into R0
+  ORR R0, R0, #0x20   @ change case
+  STR R0, [R1]        @ write char back
+
+_cipher:
+  ADD R0, R1, R2        @ add cipher shift
+  SUB S R3, R4, 0x7A    @ subtract z with flags
+  ADD GT R0, R3, 0x60   @ if R3>0 add a-1
 
 _write:
   MOV R7, #4        @ Syscall number
@@ -21,4 +33,4 @@ _exit:
 
 .data
 string:
-.ascii ""
+.ascii " "
