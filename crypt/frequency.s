dd724bb00e1eb2fb859aefd18e2d0b120f794d89
keithaclayton <36849880+keithaclayton@users.noreply.github.com>
2018-04-05 17:44:34 +0100

    Update frequency.s

--- crypt/frequency.s
+++ crypt/frequency.s
@@ -1,1 +1,2 @@
 /* program for performing an alphabet frequency attack to break ciphers and basic encyption - TBC */
+/* will focus on simply decrypting the most likely and leave dictionary resolving for later */
