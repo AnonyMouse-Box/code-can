90beb609397dcf0364ba44f3f3d9321ec4bac14f
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-04-09 14:01:54 +0100

    Update frequency.s

--- crypt/frequency.s
+++ crypt/frequency.s
@@ -5,9 +5,25 @@ _read:
   MOV R7, #3        @ Syscall number
   MOV R0, #0        @ Stdin is keyboard
   MOV R2, #1        @ read first character
-/* needs to be: 40<x<5B (send to lower) or 60<x<7B */
   LDR R1,=string    @ string placed at string:
   SWI 0
+/* need to store additional value (1-26) as R3 */
+
+_errchk0:
+  CMP R1, 0x40      @ Check not lower than A
+  B LE _error       @ If false send error
+  CMP GT R1, 0x5B   @ If true check not higher than Z
+  B GE _errchk1     @ If false send to lowercase check
+  B LT _lower       @ If true send for case lowering
+
+_errchk1:
+  CMP R1, 0x60      @ Check not lower than a
+  B LE _error       @ If false send error
+  CMP GT R1, 0x7B   @ If true check not higher than z
+  B GE _error       @ If false send error
+  B LT _cipher      @ If true send for ciphering
+
+_error:
 
 _lower:
   LDR R1,=string      @ address of char
