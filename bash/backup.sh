c0767c9948f931baa39f99d91384d9730ed7002f
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-04-27 13:14:36 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # gradually building a backup script as it will be useful to have
+
 echo building tar archive...
-tar -cf /home/user/backup.tar /source/
+tar -cvf /home/user/backup.tar /source/
+
+echo compressing files...
+bzip2 -vk backup.txt
