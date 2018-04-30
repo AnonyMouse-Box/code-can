b1ef58a4e91796b0c9d0a9eed48f113c22ad6ef0
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-04-30 21:57:15 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # gradually building a backup script as it will be useful to have
 
-/source/
-/home/user/backup.tar
-/home/user/backup.tar.bz2
-/home/user/backup.tbz2
+SRC=/source/
+BAK=/home/user/backup-$(date +%Y%m%d)
 
 echo building tar archive...
-tar -cvf /home/user/backup.tar /source/
+tar -cvf $BAK.tar $SRC
 
 echo compressing files...
-bzip2 -zvk backup.tar
+bzip2 -zvk $BAK.tar
 
 echo testing integrity...
-bzip2 -t backup.tar.bz2
+bzip2 -t $BAK.tar.bz2
 
+# when I've figued out how to read the integrity output I'll split it with an if statement here
+
+echo renaming file...
+mv $BAK.tar.bz2 $BAK.tbz2
