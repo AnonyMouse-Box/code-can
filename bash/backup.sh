d43ef1edb3cb56fe3f02d5493253d24a5562db01
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-04-30 13:02:08 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # gradually building a backup script as it will be useful to have
 
+/source/
+/home/user/backup.tar
+/home/user/backup.tar.bz2
+/home/user/backup.tbz2
+
 echo building tar archive...
 tar -cvf /home/user/backup.tar /source/
 
@@ -9,3 +14,4 @@ bzip2 -zvk backup.tar
 
 echo testing integrity...
 bzip2 -t backup.tar.bz2
+
