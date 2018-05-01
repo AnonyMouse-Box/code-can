d439adad4e700cab852319a861b0a1101cdb357f
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-05-01 12:41:35 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -8,56 +8,58 @@ MBK=/tmp/backup-$(date +%b)
 WBK=/tmp/backup-$(date +%d)
 DBK=/tmp/backup-$(date +%a)
 
-echo backup process begun $(date +%c): >> $LOG
+echo "backup process begun $(date +%c):" >> $LOG
 
-echo building tar archive... >>$LOG
+echo "building tar archive..." >>$LOG
 tar -cvf $BAK.tar $SRC >> $LOG
 echo \n  >> $LOG
 
-echo compressing files... >> $LOG
+echo "compressing files..." >> $LOG
 bzip2 -zvk $BAK.tar >> $LOG
 echo \n >> $LOG
 
-echo testing integrity... >> $LOG
+echo "testing integrity..." >> $LOG
 bzip2 -t $BAK.tar.bz2 >> $LOG
 echo \n >> $LOG
 
 # when I've figued out how to read the integrity output I'll split it with an if statement here
 # runs every day
 
-echo constructing backup schema... >> $LOG
-echo creating daily backup... >> $LOG
+echo "constructing backup schema..." >> $LOG
+echo "creating daily backup..." >> $LOG
 cp $BAK.tar.bz2 $DBK.tbz2 >> $LOG
 echo \n >> $LOG
 
-echo copying daily to server... >> $LOG
+echo "copying daily to server..." >> $LOG
 rsync $DBK.tbz2 >> $LOG
 echo \n >> $LOG
 
 # runs every 1st, 8th, 15th, 22nd and 29th
 
-echo creating weekly backup... >> $LOG
+echo "creating weekly backup..." >> $LOG
 cp $BAK.tar.bz2 $WBK.tbz2 >> $LOG
 echo \n >> $LOG
 
-echo copying weekly to server... >> $LOG
+echo "copying weekly to server..." >> $LOG
 rsync $WBK.tbz2 >> $LOG
 echo \n >> $LOG
 
-# runs every 1st
 
-echo creating monthly backup... >> $LOG
-cp $BAK.tar.bz2 $MBK.tbz2 >> $LOG
-echo \n >> $LOG
+if [ "$(date +%d)" == "01" ]
+  then
+    echo "creating monthly backup..." >> $LOG
+    cp $BAK.tar.bz2 $MBK.tbz2 >> $LOG
+    echo \n >> $LOG
 
-echo copying monthly to server... >> $LOG
-rsync $MBK >> $LOG
-echo \n >> $LOG
+    echo "copying monthly to server..." >> $LOG
+    rsync $MBK >> $LOG
+    echo \n >> $LOG
+fi
 
-echo cleaning up temporary files... >> $LOG
+echo "cleaning up temporary files..." >> $LOG
 rm $BAK.tar $BAK.tar.bz2 $DBK.tbz2 $WBK.tbz2 $MBK.tbz2 >> $LOG
 echo \n >> $LOG
 
-echo backup complete $(date +%c). >> $LOG
+echo "backup complete $(date +%c)." >> $LOG
 echo \n >> $LOG
 echo \n >> $LOG
