2bf77f2f4993fbda6142dfb168059a58038b15fd
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-05-10 21:15:59 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -25,22 +25,22 @@ if [ "$USER" == "root" ];
         mkdir -p /home/admin
     fi
 fi
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 echo "backup process begun $(date +%c):" &>> $LOG
 
 echo "building archive..." &>> $LOG
 tar --exclude="$LOG" -cpvf $BAK.tar $SRC &>> $LOG
-echo "\n"  &>> $LOG
+echo &>> $LOG
 
 echo "compressing files..." &>> $LOG
 bzip2 -zvk $BAK.tar &>> $LOG
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 echo "testing integrity..." &>> $LOG
 bzip2 -vt $BAK.tar.bz2 &>> $LOG
 TST="$(tail -n 1 $LOG)"
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 if [ "$TST" != "  $BAK.tar.bz2: ok" ];
   then
@@ -55,38 +55,38 @@ fi
 echo "constructing backup schema..." &>> $LOG
 echo "creating daily backup..." &>> $LOG
 cp -v $BAK.tar.bz2 $DBK.tbz2 &>> $LOG
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 echo "copying daily to server..." &>> $LOG
 rsync -htvpEogSm $DBK.tbz2 $USR@$HST:$DST &>> $LOG
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 if [ "$(date +%d)" == "01" ] || [ "$(date +%d)" == "08" ] || [ "$(date +%d)" == "15" ] || [ "$(date +%d)" == "22" ] || [ "$(date +%d)" == "29" ];
   then    
     echo "creating weekly backup..." &>> $LOG
     cp -v $BAK.tar.bz2 $WBK.tbz2 &>> $LOG
-    echo "\n" &>> $LOG
+    echo &>> $LOG
     
     echo "copying weekly to server..." &>> $LOG
     rsync -htvpEogSm $WBK.tbz2 $USR@$HST:$DST &>> $LOG
-    echo "\n" &>> $LOG
+    echo &>> $LOG
     
     if [ "$(date +%d)" == "01" ];
       then
         echo "creating monthly backup..." &>> $LOG
         cp -v $BAK.tar.bz2 $MBK.tbz2 &>> $LOG
-        echo "\n" &>> $LOG
+        echo &>> $LOG
         
         echo "copying monthly to server..." &>> $LOG
         rsync -htvpEogSm $MBK.tbz2 $USR@$HST:$DST &>> $LOG
-        echo "\n" &>> $LOG
+        echo &>> $LOG
     fi
 fi
 
 echo "cleaning up temporary files..." &>> $LOG
 rm -v $BAK.tar $BAK.tar.bz2 $DBK.tbz2 $WBK.tbz2 $MBK.tbz2 &>> $LOG
-echo "\n" &>> $LOG
+echo &>> $LOG
 
 echo "backup complete :) $(date +%c)." &>> $LOG
-echo "\n" &>> $LOG
-echo "\n" &>> $LOG
+echo &>> $LOG
+echo &>> $LOG
