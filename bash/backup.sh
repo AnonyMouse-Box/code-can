016f2b95e8f94b0a5822f10bd7f98b784ead1ac6
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-05-10 21:42:14 +0100

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # gradually building a backup script as it will be useful to have -vrpEogtSxhm
 
+t="0"
 USR="$USER"
 HST="127.0.0.1"
 SRC="$1"
@@ -12,12 +13,12 @@ WBK="/tmp/backup-$(date +%d)"
 DBK="/tmp/backup-$(date +%a)"
 
 echo "preparing variables.." &> $LOG
-if [ "$#" == "0" ];
+if [ $# == "0" ];
   then
     SRC="/home"
 fi
 
-if [ "$USER" == "root" ];
+if [ $USER == "root" ];
   then
     USR="admin"
     if [ ! -d /home/admin ];
@@ -29,64 +30,72 @@ echo &>> $LOG
 
 echo "backup process begun $(date +%c):" &>> $LOG
 
-echo "building archive..." &>> $LOG
-tar --exclude="$LOG" -cpvf $BAK.tar $SRC &>> $LOG
-echo &>> $LOG
+while [ $t -lt "3" ]; do
+  
+  echo "building archive..." &>> $LOG
+  tar --exclude="$LOG" -cpvf $BAK.tar $SRC &>> $LOG
+  echo &>> $LOG
+  
+  echo "compressing files..." &>> $LOG
+  bzip2 -zvk $BAK.tar &>> $LOG
+  echo &>> $LOG
+  
+  echo "testing integrity..." &>> $LOG
+  bzip2 -vt $BAK.tar.bz2 &>> $LOG
+  TST="$(tail -n 1 $LOG)"
+  echo &>> $LOG
+  
+  if [ "$TST" != "  $BAK.tar.bz2: ok" ];
+    then
+      echo "failed integrity test" &>> $LOG
+      let "t += 1"
+      sleep 300
+      echo "retrying..." &>> $LOG
+  
+  fi
 
-echo "compressing files..." &>> $LOG
-bzip2 -zvk $BAK.tar &>> $LOG
-echo &>> $LOG
-
-echo "testing integrity..." &>> $LOG
-bzip2 -vt $BAK.tar.bz2 &>> $LOG
-TST="$(tail -n 1 $LOG)"
-echo &>> $LOG
-
-if [ "$TST" != "  $BAK.tar.bz2: ok" ];
-  then
-    echo "failed integrity test" &>> $LOG
-    echo "retrying..." &>> $LOG
-    echo "failed too many times..." &>> $LOG
-    echo "removing files and giving up :(" &>> $LOG
-fi
-
-# when I've figued out how to read the integrity output I'll split it with an if statement here
-
-echo "constructing backup schema..." &>> $LOG
-echo "creating daily backup..." &>> $LOG
-cp -v $BAK.tar.bz2 $DBK.tbz2 &>> $LOG
-echo &>> $LOG
-
-echo "copying daily to server..." &>> $LOG
-rsync -htvpEogSm $DBK.tbz2 $USR@$HST:$DST &>> $LOG
-echo &>> $LOG
-
-if [ "$(date +%d)" == "01" ] || [ "$(date +%d)" == "08" ] || [ "$(date +%d)" == "15" ] || [ "$(date +%d)" == "22" ] || [ "$(date +%d)" == "29" ];
-  then    
-    echo "creating weekly backup..." &>> $LOG
-    cp -v $BAK.tar.bz2 $WBK.tbz2 &>> $LOG
-    echo &>> $LOG
+  # when I've figued out how to read the integrity output I'll split it with an if statement here
+  
+  echo "constructing backup schema..." &>> $LOG
+  echo "creating daily backup..." &>> $LOG
+  cp -v $BAK.tar.bz2 $DBK.tbz2 &>> $LOG
+  echo &>> $LOG
+  
+  echo "copying daily to server..." &>> $LOG
+  rsync -htvpEogSm $DBK.tbz2 $USR@$HST:$DST &>> $LOG
+  echo &>> $LOG
+  
+  if [ "$(date +%d)" == "01" ] || [ "$(date +%d)" == "08" ] || [ "$(date +%d)" == "15" ] || [ "$(date +%d)" == "22" ] || [ "$(date +%d)" == "29" ];
+    then    
+      echo "creating weekly backup..." &>> $LOG
+      cp -v $BAK.tar.bz2 $WBK.tbz2 &>> $LOG
+      echo &>> $LOG
+      
+      echo "copying weekly to server..." &>> $LOG
+      rsync -htvpEogSm $WBK.tbz2 $USR@$HST:$DST &>> $LOG
+      echo &>> $LOG
+      
+      if [ "$(date +%d)" == "01" ];
+        then
+          echo "creating monthly backup..." &>> $LOG
+          cp -v $BAK.tar.bz2 $MBK.tbz2 &>> $LOG
+          echo &>> $LOG
+          
+          echo "copying monthly to server..." &>> $LOG
+          rsync -htvpEogSm $MBK.tbz2 $USR@$HST:$DST &>> $LOG
+          echo &>> $LOG
+      fi
+  fi
+  
+  echo "cleaning up temporary files..." &>> $LOG
+  rm -v $BAK.tar $BAK.tar.bz2 $DBK.tbz2 $WBK.tbz2 $MBK.tbz2 &>> $LOG
+  echo &>> $LOG
+  
+  echo "backup complete :) $(date +%c)." &>> $LOG
+  echo &>> $LOG
+  echo &>> $LOG
+  
+done    
     
-    echo "copying weekly to server..." &>> $LOG
-    rsync -htvpEogSm $WBK.tbz2 $USR@$HST:$DST &>> $LOG
-    echo &>> $LOG
-    
-    if [ "$(date +%d)" == "01" ];
-      then
-        echo "creating monthly backup..." &>> $LOG
-        cp -v $BAK.tar.bz2 $MBK.tbz2 &>> $LOG
-        echo &>> $LOG
-        
-        echo "copying monthly to server..." &>> $LOG
-        rsync -htvpEogSm $MBK.tbz2 $USR@$HST:$DST &>> $LOG
-        echo &>> $LOG
-    fi
-fi
-
-echo "cleaning up temporary files..." &>> $LOG
-rm -v $BAK.tar $BAK.tar.bz2 $DBK.tbz2 $WBK.tbz2 $MBK.tbz2 &>> $LOG
-echo &>> $LOG
-
-echo "backup complete :) $(date +%c)." &>> $LOG
-echo &>> $LOG
-echo &>> $LOG
+echo "failed too many times..." &>> $LOG
+echo "removing files and giving up :(" &>> $LOG
