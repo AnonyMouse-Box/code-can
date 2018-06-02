1987209c35e10beebaf6fa8406e763426c6f327c
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-06-02 22:10:14 +0100

    Update backup.py

--- python/backup.py
+++ python/backup.py
@@ -2,7 +2,7 @@
 # backup.sh converted into python
 # backup.sh Files User Remote Folder Log
 import subprocess
-tar --exclude="$3" -cpvf "$2" "$1" &>> "$TMP"
-bzip2 -zvk "$1.tar" &>> "$TMP"
-bzip2 -vt "$1" &>> "$TMP"
-rsync -htvpEogSm "$DBK.tbz2" "$USER@$HST:$DST" &>> "$TMP"
+# tar --exclude="$3" -cpvf "$2" "$1" &>> "$TMP"
+# bzip2 -zvk "$1.tar" &>> "$TMP"
+# bzip2 -vt "$1" &>> "$TMP"
+# rsync -htvpEogSm "$DBK.tbz2" "$USER@$HST:$DST" &>> "$TMP"
