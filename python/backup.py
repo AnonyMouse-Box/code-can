533ef14d17e408fae05fe661515c4f8c18819ce8
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-06-02 22:43:15 +0100

    Update backup.py

--- python/backup.py
+++ python/backup.py
@@ -2,6 +2,7 @@
 # backup.sh converted into python
 # backup.sh Files User Remote Folder Log
 import subprocess
+import argparse
 subprocess.run(["tar", "--exclude='$3'", "-cpvf", "'$2'", "'$1'", "&>>", "'$TMP'"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 subprocess.run(["bzip2", "-zvk", "'$1.tar'", "&>>", "'$TMP'"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 subprocess.run(["bzip2", "-vt", "'$1'", "&>>", "'$TMP'"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
