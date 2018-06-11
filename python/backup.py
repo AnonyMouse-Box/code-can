74e5d0abcf1ea21d1f4f1932bf6fdda02ba30952
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-06-11 12:36:01 +0100

    Update backup.py

--- python/backup.py
+++ python/backup.py
@@ -5,4 +5,21 @@ for e in (0, 3):
   archive
   compress
   integrity
-  break
+  copy daily
+  sync daily
+  copy weekly
+  sync weekly
+  copy monthly
+  sync monthly
+  archive old logs
+  compress old logs
+  integrity old logs
+  copy old logs to archive
+  clear log folder
+  copy log
+  remove temp files
+  exit successful
+  
+copy log
+remove temp files
+exit unsuccessful
