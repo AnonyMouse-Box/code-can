91c0a0a513a0220aa853cfaff59713a38b666193
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-06-11 13:04:07 +0100

    Update backup2.sh

--- bash/backup2.sh
+++ bash/backup2.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-while $? != 0;
-  do
+# backup2.sh [remote IP]
+
+function IsAlive(){
+ping -c 5 "$1"
+for i in {1..12};
+  then
+    if [ $? != 0 ];
+      then
+        sleep 300
+        ping -c 5 "$1"
+      else
+        exit 0
+    done
+done
+exit 1
+}
+
 for i in {1..3};
   then
+    IP="$1"
+    IsAlive "$IP"
+    if [ "$?" == "1" ];
+      then
+        break
+    fi
 done
