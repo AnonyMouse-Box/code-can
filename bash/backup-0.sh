d66ea137b3620a11016eaaebc34adb6d266dd9b9
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-06-12 12:44:32 +0100

    Update backup-0.sh

--- bash/backup-0.sh
+++ bash/backup-0.sh
@@ -2,26 +2,37 @@
 # backup2.sh [remote IP]
 
 function IsAlive(){
-ping -c 5 "$1"
-for i in {1..12};
-  then
-    if [ $? != 0 ];
-      then
-        sleep 300
-        ping -c 5 "$1"
-      else
-        exit 0
-    done
-done
-exit 1
+  ping -c 5 '$1'
+  for i in {1..12};
+    then
+      if [ $? != 0 ];
+        then
+          sleep 300
+          ping -c 5 '$1'
+        else
+          exit 0
+      done
+  done
+  exit 1
 }
 
 for i in {1..3};
   then
-    IP="$1"
-    IsAlive "$IP"
-    if [ "$?" == "1" ];
+    IP='$1'
+    IsAlive '$IP'
+    if [ '$?' == '1' ];
       then
-        break
+        ERR='0'
+        continue
     fi
 done
+
+case $ERR in
+  '0')
+    echo 'could not connect to remote server' &>> [log file]
+    ;;
+  *)
+    echo 'critical failure: undesignated error' &>> [log file]
+    exit 1
+    ;;
+  esac
