fb6bd30617d668cb706df80f2fd2f85a1ee402f2
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-07-24 13:11:16 +0100

    Update 'bash/idgen.sh'

--- bash/idgen.sh
+++ bash/idgen.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 ID="/tmp/id.txt"
 LS="idlist.txt"
-tr -dc A-Za-z0-9 < /dev/urandom | head -c 64 | sed -r "s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/\1-\2-\3-\4 : \5-\6-\7-\8/" > id.txt
-if [ -z "$(cat id.txt | grep idlist.txt)" ] ;
+for i in {1..3} ;
  then
-  cat id.txt >> idlist.txt
-  echo id.txt
-fi
-exit 0
+  tr -dc A-Za-z0-9 < /dev/urandom | head -c 64 | sed -r "s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/\1-\2-\3-\4 : \5-\6-\7-\8/" > $ID
+  if [ -z "$(cat $ID | grep $LS )" ] ;
+   then
+    cat $ID >> $LS
+    echo $ID
+    rm $ID
+    exit 0
+  fi
+done
+exit 1
