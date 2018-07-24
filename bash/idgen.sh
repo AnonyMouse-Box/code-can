ae03af72177d89ff0c3e37530db7d292106b5aff
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-07-24 13:03:06 +0100

    Update 'bash/idgen.sh'

--- bash/idgen.sh
+++ bash/idgen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 tr -dc A-Za-z0-9 < /dev/urandom | head -c 64 | sed -r "s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/\1-\2-\3-\4 : \5-\6-\7-\8/" > id.txt
-cat id.txt | grep idlist.txt
+if [ -z "$cat id.txt | grep idlist.txt" ] ;
+	then
+    	cat id.txt >> idlist.txt
+fi
 exit 0
