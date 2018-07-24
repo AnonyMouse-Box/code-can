03f2b790210dd47891f86df581c99f70a41a9a3d
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-07-24 13:14:38 +0100

    Update 'bash/idgen.sh'

--- bash/idgen.sh
+++ bash/idgen.sh
@@ -6,12 +6,9 @@ for i in {1..3} ;
   tr -dc A-Za-z0-9 < /dev/urandom | head -c 64 | sed -r "s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/\1-\2-\3-\4 : \5-\6-\7-\8/" > $ID
   if [ -z "$(cat $ID | grep $LS )" ] ;
    then
-    cat $ID >> $LS
-    cat $ID
-    rm $ID
+    cat $ID >> $LS ; cat $ID ; rm $ID
     exit 0
   fi
 done
-rm $ID
-echo "duplication error: check config or try again."
+rm $ID ; echo "duplication error: check config or try again."
 exit 1
