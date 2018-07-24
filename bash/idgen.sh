3c64b75211afe7394268ee3106bd2546ca48c053
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-07-24 09:02:18 +0100

    Update 'bash/idgen.sh'

--- bash/idgen.sh
+++ bash/idgen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 tr -dc A-Za-z0-9 < /dev/urandom | head -c 64 | sed -r "s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/\1-\2-\3-\4 : \5-\6-\7-\8/" > id.txt
+cat id.txt | grep idlist.txt
 exit 0
