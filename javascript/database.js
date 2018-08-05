168536db324c28377b7e3e1a67fe91fc1d8a7213
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-08-05 16:03:09 +0100

    Add 'javascript/database.js'

---
+++ javascript/database.js
@@ -0,0 +1,30 @@
+let person = {
+  _name,
+  _dob,
+  _age,
+  get name() {
+    return this._name;
+  },
+  get dob() {
+    return this._dob;
+  },
+  get age() {
+    return this._age;
+  },
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+      return `Name was set to ${newName}.`;
+    } else {
+      return 'Invalid input.';
+    }
+  },
+  set dob(newDoB) {
+    if (typeof newDoB === '') {
+      this._dob = newDoB;
+      return `${this._name}'s Date of Birth set to {newDoB}.`;
+    } else {
+      return 'Invalid input.';
+    }
+  }
+}
