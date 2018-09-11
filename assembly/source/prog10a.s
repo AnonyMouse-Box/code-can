b2ec507ea3e32e28c45b3dbb9e8b37ca7daf0b60
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-09-11 12:54:12 +0100

    Update 'assembly/source/prog10a.s'

--- assembly/source/prog10a.s
+++ assembly/source/prog10a.s
@@ -1,2 +1,9 @@
 /**** Convert to binary for printing ****/
-.global _start
+  .global _start
+_start:
+  MOV R6, #215          @ Number to print in R6
+  MOV R10, #1           @ set up mask
+  MOV R9, R10, LSL #31
+  LDR R1, = string      @ Point R1 to string
+  
+_bits:
