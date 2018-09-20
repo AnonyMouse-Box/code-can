584285ec62dcd67475640f742411397136c98e6c
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-09-20 21:36:14 +0100

    Update 'javascript/click.js'

--- javascript/click.js
+++ javascript/click.js
@@ -1,7 +1,7 @@
 const progress = (id) => {
-  if(document.getElementById('demo').value === 100){
+  if(document.getElementById('id').value === 100){
     window.alert('Complete!');
   } else {
-    document.getElementById('demo').value ++;
+    document.getElementById('id').value ++;
   }
 }
