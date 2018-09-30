51495c7aeb1f46d70a1a0954a898d8536bd77ef5
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-09-30 21:14:17 +0100

    Update 'bash/update.sh'

--- bash/update.sh
+++ bash/update.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
+function timestamp() {
+  while IFS= read -r line; do
+    echo [$(date +'%F %T.%N')] $line
+  done
+}
 # redirect the stdout/stderr to screen AND log file
 LOG="/var/log/usr/update.log"
 DIR=$(mktemp -d)
 if [ ${#DIR} == 19 ]; then
   mkfifo ${DIR}/$$-err ${DIR}/$$-out
   # to merge stdout/stderr to log file AND screen
-  ( exec tee -a ${LOG} <${DIR}/$$-out ) &
-  ( exec tee -a ${LOG} <${DIR}/$$-err >&2 ) &
+  ( exec tee -a ${LOG} < ${DIR}/$$-out ) &
+  ( exec tee -a ${LOG} < ${DIR}/$$-err >&2 ) &
   # redirect stdout/stderr
-  exec 1> ${DIR}/$$-out
-  exec 2> ${DIR}/$$-err
+  exec 1> >( timestamp ${DIR}/$$-out > ${DIR}/$$-out )
+  exec 2> >( timestamp ${DIR}/$$-err > ${DIR}/$$-err )
   
   # check network access, ping gateway
   VAR=0
