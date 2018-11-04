7665587e18c8f0f1adf3b6542c672ca79c6d2445
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-11-04 15:23:02 +0000

    Update 'bash/redirect.sh'

--- bash/redirect.sh
+++ bash/redirect.sh
@@ -26,13 +26,13 @@ if [ ${#DIR} == 19 ]; then
   END=$(date +%s)
   
   # calculate time taken
-  seconds=$(echo "$end - $start" | bc)
-  if (( $SECONDS > 3600 )) ; then
+  SECONDS=$(echo "$END - $START" | bc)
+  if [ $SECONDS > 3600 ]; then
     let "hours=SECONDS/3600"
     let "minutes=(SECONDS%3600)/60"
     let "seconds=(SECONDS%3600)%60"
     echo "Completed in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
-  elif (( $SECONDS > 60 )) ; then
+  elif [ $SECONDS > 60 ]; then
     let "minutes=(SECONDS%3600)/60"
     let "seconds=(SECONDS%3600)%60"
     echo "Completed in $minutes minute(s) and $seconds second(s)"
