51b40c807ffe33b20db8a7f28cc8e9b4dc72ae3d
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2018-12-10 13:02:35 +0000

    Update 'bash/backup-0.sh'

--- bash/backup-0.sh
+++ bash/backup-0.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+function timestamp() {
+  while IFS= read -r line; do
+    echo [$(date +"%F %T.%N")] $line
+  done
+}
+# redirect the stdout/stderr to screen AND log file
+LOG="/var/log/usr/file.log"
+DIR=$(mktemp -d)
+if [ ${#DIR} == 19 ]; then
+  mkfifo ${DIR}/$$-err ${DIR}/$$-out
+  # to merge stdout/stderr to log file AND screen
+  ( exec tee -a ${LOG} < ${DIR}/$$-out ) &
+  ( exec tee -a ${LOG} < ${DIR}/$$-err >&2 ) &
+  # redirect stdout/stderr
+  exec 1> >( timestamp ${DIR}/$$-out > ${DIR}/$$-out )
+  exec 2> >( timestamp ${DIR}/$$-err > ${DIR}/$$-err )
+  
+  START=$(date +%s)
+  echo ">>>START OF OUTPUT<<<"
+
 # backup-0.sh [backup source] [user] [remote IP] [destination folder] [log folder]
 # defaults    [/home] [current] [127.0.0.1] [/mnt/backup] [/home/'user'/log]
 
