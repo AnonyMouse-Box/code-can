40c22caad834e3afaff0b148feb4cb2a7b7435ea
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2019-01-09 12:56:26 +0000

    Update 'python/random.py'

--- python/random.py
+++ python/random.py
@@ -37,16 +37,27 @@ class coin:
   
 
 class die:
-  def __init__(self, name, faces):
+  def __init__(self, name, start, stop, step):
     self.name = name
-    self.weights = {a = 1 for a in range(faces)}    
+    self.faces = int((stop - start) / step)
+    self.weights = {a = 1 for a in range(start, stop, step)}    
+    return;
+    
+  def __setWeight(self, value, weight):
+    self.weights[value] = weight
     return;
     
   def __rollDie(self):
-    sides = int((stop - start) / step)
-    print("you roll a D{0}".format(sides))
-    result = random.randrange(start, stop, step)
-    print("you rolled a {0}".format(die))
+    print("you roll a D{0}".format(self.faces))
+    for a in self.weight.values():
+      n += a
+    pseudodie = random.randrange(n)
+    for key, value in self.weight.items():
+      valueSum += value
+      if pseudodie <= valueSum:
+        result = key
+        print("you rolled a {0}".format(result))
+        break
     return result;
 
 
