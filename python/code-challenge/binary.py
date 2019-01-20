fd76be989d0ba2f4af6ae136aac1759d0da70a97
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2019-01-20 21:13:41 +0000

    Update 'python/code-challenge/binary.py'

--- python/code-challenge/binary.py
+++ python/code-challenge/binary.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 def isBinary(text):
-  for letter in text:
-    if letter == "1" or letter == "0":
-      continue
-    else:
-      return False
-  return True
+  if isinstance(text, str):
+    for letter in text:
+      if letter == "1" or letter == "0":
+        continue
+      else:
+        return False
+    return True
+  else:
+    return "is not a string"
