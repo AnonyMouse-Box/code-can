ebc7a539be57995575f0eeb0b0e2ce6b2dd22b3a
C.Monkey <hogwashga@noreply@deskpotato.co.uk>
2019-01-24 20:27:38 +0000

    Update 'python/code-challenge/temp.py'

--- python/code-challenge/temp.py
+++ python/code-challenge/temp.py
@@ -2,11 +2,11 @@ def appendDigits(n, base):
   new = []
   for item in base:
     for value in range(n):
-      new.append(item.append(value))
+      new.append(item + [value,])
   return new
 
 def calculateSetOfSets(n):
   base = ([],)
   for value in range(n):
-    setOfSets = appendDigits(n, base)
-  return setOfSets
+    base = appendDigits(n, base)
+  return base
