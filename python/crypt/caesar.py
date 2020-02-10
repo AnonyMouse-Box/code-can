b7fc140774a4c19234fb74424a18014b777c86fd
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-10 14:16:28 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -4,10 +4,16 @@ try:
     if isinstance(input, str):
       output = []
       for character in input:
-        value = ord(character)
-        if value is in range(20,95): # (unicode decimal) make language adjustments here
+        value = ord('character')
+        if value is in range(48,58): # (unicode decimal) scrambles digits
           
-        output.append(character)
+        elif value is in range(65,91): # (unicode decimal) make uppercase language adjustments here
+          
+        elif value is in range(97,123): # (unicode decimal) make lowercase language adjustments here
+          
+        else:
+          newCharacter = character
+        output.append(newCharacter)
       return output
     else:
       raise ValueError("input must be a string!")
