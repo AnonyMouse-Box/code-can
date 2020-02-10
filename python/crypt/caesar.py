56f762ae129ac2024b9dc3c6901a86f169e04b5e
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-10 12:33:41 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-def caesar(input):
-  output = []
-  for character in input:
-    output.append(character)
-  return output
+try:
+  def caesar(input):
+    output = []
+    for character in input:
+      output.append(character)
+    return output
+except:
+  print("An error has occurred.") 
