a152b5640dce1fa401b95f2179fc8918c0571e09
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-11 12:25:44 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 try:
+  digits = (48,58) # (unicode decimal)
+  upper = (65,91) # (unicode decimal) make uppercase language adjustments here
+  lower = (97,123) # (unicode decimal) make lowercase language adjustments here
+  
   def caesar(input):
     if isinstance(input, str):
       output = []
       for character in input:
         value = ord('character')
-        if value is in range(48,58): # (unicode decimal) scrambles digits
+        if value is in range(digits): # scrambles digits
           newCharacter = character
-        elif value is in range(65,91): # (unicode decimal) make uppercase language adjustments here
+        elif value is in range(upper): 
           newCharacter = character
-        elif value is in range(97,123): # (unicode decimal) make lowercase language adjustments here
+        elif value is in range(lower): 
           newCharacter = character
         else:
           newCharacter = character
