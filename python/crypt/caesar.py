edc22f8ee1be5e91377e58ec1fe77142700c514f
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-11 13:06:04 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 try:
-  digits = (48,58) # (unicode decimal)
-  upper = (65,91) # (unicode decimal) make uppercase language adjustments here
-  lower = (97,123) # (unicode decimal) make lowercase language adjustments here
+  numerals = [48,58] # (unicode decimal)
+  big = [65,91] # (unicode decimal) make uppercase language adjustments here
+  little = [97,123] # (unicode decimal) make lowercase language adjustments here
+  digits = range(numerals[0],numerals[1])
+  upper = range(big[0],big[1])
+  lower = range(little[0],little[1])
+  
+  def rotate(character):
+    return character
   
   def caesar(input):
     if isinstance(input, str):
-      output = []
+      output = ""
       for character in input:
         value = ord('character')
-        if value is in range(digits): # scrambles digits
-          newCharacter = character
-        elif value is in range(upper): 
-          newCharacter = character
-        elif value is in range(lower): 
-          newCharacter = character
+        if value is in digits: # scrambles digits
+          newCharacter = rotate(character)
+        elif value is in upper: 
+          newCharacter = rotate(character)
+        elif value is in lower: 
+          newCharacter = rotate(character)
         else:
           newCharacter = character
-        output.append(newCharacter)
+        output += newCharacter
       return output
     else:
       raise ValueError("input must be a string!")
+    
 except:
   print("An error has occurred.") 
+  
 else:
   print("Cipher complete.")
