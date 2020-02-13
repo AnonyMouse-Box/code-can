09b6a7b40655ce35e9e65fbe048df896fd18cde9
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-13 13:32:21 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python3
-try:
-  plainTextUpper = [chr(i) for i in range(ord('A'),ord('Z')+1)]
-  plainTextLower = [chr(i) for i in range(ord('a'),ord('z')+1)]
-  cipherText = plainTextUpper
-  
-  def rotate(cipherTextList, rotation):
-    for value in cipherTextList:
-      newValue = ord('value') + rotation
-      cipherTextList[cipherTextList.index(value)] = newValue
-    return
-  
-  def caesar(text, rotation):
-    if isinstance(text, str):
-      if isinstance(rotation, int):
-        rotate(cipherText, rotation)
-        cipher = []
-        textList = list(text)
-        for character in textList:
-          if character is in plainTextUpper or character is in plainTextLower:
-            newCharacter = cipherText[plainTextUpper.index(character.upper())]
-            if character is in plainTextLower:
-              newCharacter = newCharacter.lower()
-          else:
-            newCharacter = character
-          cipher.append(newCharacter)
-          output = ''.join(cipher)
-        return output
-      else:
-        raise ValueError("rotation must be an integer!")
+plainTextUpper = [chr(i) for i in range(ord('A'),ord('Z')+1)]
+plainTextLower = [chr(i) for i in range(ord('a'),ord('z')+1)]
+cipherText = plainTextUpper
+
+def rotate(cipherTextList, rotation):
+  for value in cipherTextList:
+    newValue = ord('value') + rotation
+    cipherTextList[cipherTextList.index(value)] = newValue
+  return
+
+def caesar(text, rotation):
+  if isinstance(text, str):
+    if isinstance(rotation, int):
+      rotate(cipherText, rotation)
+      cipher = []
+      textList = list(text)
+      for character in textList:
+        if character is in plainTextUpper or character is in plainTextLower:
+          newCharacter = cipherText[plainTextUpper.index(character.upper())]
+          if character is in plainTextLower:
+            newCharacter = newCharacter.lower()
+        else:
+          newCharacter = character
+        cipher.append(newCharacter)
+        output = ''.join(cipher)
+      return output
     else:
-      raise ValueError("input must be a string!")
-    
-except:
-  print("An error has occurred.") 
-  
-else:
-  print("Cipher complete.")
+      raise ValueError("rotation must be an integer!")
+  else:
+    raise ValueError("input must be a string!")
