eddfe10a3ae329e35b3c85eec40764b7d457ceeb
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-21 14:58:41 +0000

    Update caesar.py

--- python/crypt/caesar.py
+++ python/crypt/caesar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-class caesar:
+class caesar(crypt):
   def __init__(self, name, rotation, flagDigits):
     if isinstance(rotation, int):
       if isinstance(flagDigits, bool):
