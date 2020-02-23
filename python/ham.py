f822975d7e9e01e509a6c0de4a5ef5821a2ce4e3
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-23 22:27:18 +0000

    Update ham.py

--- python/ham.py
+++ python/ham.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
-class ham(object):
-  def __init__(self):
+class errorCorrect(object):
+  def __init__(self, bits):
+    if isinstance(bits, int):
+      self.bits = bits
+    else:
+      raise
    return;
+  
+  def ham(binary):
+    return;
+  
+  def deham(binary):
+    return;
