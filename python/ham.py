75d3b0dcb87d7aec233a139b9b9f3ca56f1fd599
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-23 22:17:01 +0000

    Update ham.py

--- python/ham.py
+++ python/ham.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+import uuid
+from . import super
+from . import sub
+
+def __generateID(type):
+  check = True
+  while check == True
+    id = uuid.uuid4()
+    check = id in type.dict
+  return id;
+  
+def createObject(type):
+  name = __generateID(type)
+  return type(name);
+
+superclass.dict = {}
+subclass.dict = {}
 
 class superclass(object):
   def __init__(self, name):
