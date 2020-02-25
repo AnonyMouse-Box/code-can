740c139d768841e7fc1f59d4dad7b34720cf6f92
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-25 12:52:30 +0000

    Update ham.py

--- python/ham.py
+++ python/ham.py
@@ -4,39 +4,43 @@ class ham(object):
   def __init__(self, bits = 31):
     if isinstance(bits, int):
       self.bits = bits
+      self.code = self.__code(bits)
     else:
-      raise TypeError('no. of bits must be an integer')
+      raise TypeError('total no. of bits must be an integer')
     return;
   
-  def __split(binary):
+  def __code(bits): # calculate number of code bits
     return output;
   
-  def __pad(binary):
+  def __split(binary): # split binary into list of data bit segments
     return output;
   
-  def __calcParity(array):
+  def __pad(binary): # pad the last data bit segment to correct length
     return output;
   
-  def __insert(array, parity):
+  def __calcParity(array): # calculate the parity bits required for each segment
     return output;
   
-  def __validate(array):
+  def __insert(array, parity): # combine the code and data bits
     return output;
   
-  def __correct(array, valid):
+  def __validate(array): # check each of the parities
     return output;
   
-  def __remove(array):
+  def __correct(array, valid): # correct any errors found
     return output;
   
-  def ham(binary):
+  def __remove(array): # remove the code bits
+    return output;
+  
+  def ham(binary): # take in data and turn it into a coded list
     split = self.__split(binary)
     split += self.__pad(split[-1])
     parity = self.__calcParity(split)
     output = self.__insert(split, parity)
     return output;
   
-  def deham(array):
+  def deham(array): # take in a coded list and turn it into data
     self.bits = len(array[0])
     valid = self.__validate(array)
     corrected = self.__correct(array, valid)
