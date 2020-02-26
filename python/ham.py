cec3186b68fd2521940023983ffc29553a593a1b
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-02-26 12:30:27 +0000

    Update ham.py

--- python/ham.py
+++ python/ham.py
@@ -16,17 +16,17 @@ class ham(object):
   def __code(bits): # calculate number of code bits
     return output;
   
-  def __split(data): # split binary into list of data bit segments
+  def __split(raw): # split binary into list of data bit segments
     output = []
-    while data is not "":
-      output += data[:self.data]
-      data = data[self.data:]
+    while raw is not "":
+      output += raw[:self.data]
+      raw = raw[self.data:]
     return output;
   
-  def __pad(data): # pad the last data bit segment to correct length
-    while len(data) is not self.data:
-      data += "0"
-    return data;
+  def __pad(raw): # pad the last data bit segment to correct length
+    while len(raw) is not self.data:
+      raw += "0"
+    return raw;
   
   def __calcParity(array): # calculate the parity bits required for each segment
     return output;
@@ -43,17 +43,17 @@ class ham(object):
   def __remove(array): # remove the code bits
     return output;
   
-  def __concatenate(array): # concatenate the list into data
+  def __concatenate(array): # concatenate the list into raw data
     return output;
   
-  def ham(data): # take in data and turn it into a coded list
-    split = self.__split(data)
+  def ham(raw): # take in raw data and turn it into a coded list
+    split = self.__split(raw)
     padded = split[:-1] + self.__pad(split[-1])
     parity = self.__calcParity(padded)
     output = self.__insert(split, parity)
     return output;
   
-  def deham(array): # take in a coded list and turn it into data
+  def deham(array): # take in a coded list and turn it into raw data
     self.bits = len(array[0])
     valid = self.__validate(array)
     corrected = self.__correct(array, valid)
