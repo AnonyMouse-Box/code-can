24e4832fef28aa6666a9806915f491de66c761b6
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-03-02 13:05:29 +0000

    Update ham.py

--- python/ham.py
+++ python/ham.py
@@ -38,8 +38,9 @@ class ham(object):
     return code;
   
   def __explode(): # explode code bits list into a matrix of the values they validate
-    for value in self.code:
-      item = [] # set up list for each codebit consider how to store
+    store = {}
+    for item in self.code:
+      store.update( { item : [] } ) # set up list for each codebit consider how to store
     for n in range(self.bits + 1):
       value = n
       while value > 0:
@@ -48,8 +49,8 @@ class ham(object):
           p + 1
         p - 1
         value -= 2**p
-        log = 2**p # log n against 2**p list
-    return output;  
+        store.setdefault(2**p, []).append(n) # log n against 2**p list
+    return store;  
   
   def __bits(bits): # define the code variables
     if isinstance(bits, int):
