af8f7c2265bdf9afdbcd0a13b12662f2b50fb86a
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-03-27 19:34:25 +0000

    Update haspet.hs

--- haskell/haspet.hs
+++ haskell/haspet.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env runhaskell
 
-main = putStrLn "Welcome to HasPet!"
+main = print "Welcome to HasPet!"
 
 name = "Spike"
 age = 5
@@ -8,5 +8,5 @@ weight = 9.5
 hungry = True
 photo = (=^o.o^=)__
 
-main = putStrLn "Hello " + name
-main = putStrLn photo
+main = print "Hello " + name
+main = print photo
