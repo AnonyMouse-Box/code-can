618cae042c28b839836b255d2df695e6de1654a4
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-03-29 13:23:29 +0100

    Update haspet.hs

--- haskell/haspet.hs
+++ haskell/haspet.hs
@@ -3,11 +3,15 @@
 main = do
   putStrLn "Welcome to HasPet!"
   
+  putStr "Enter name: "
   name <- getLine -- String
+  putStr "Enter age: "
   age <- getLine -- Int
+  putStr "Enter weight: "
   weight <- getLine -- Float
+  putStr "Is your pet hungry? "
   hungry <- getLine -- Bool
   let photo = "(=^o.o^=)__" -- Selection
   
-  putStrLn ("Hello " ++ name)
+  putStrLn $ "Hello " ++ name
   putStrLn photo
