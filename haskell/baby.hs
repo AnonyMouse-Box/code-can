307e895cc9903ac159d7ed32efce8a5379dd9294
AnonyMouse-Box <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-04-12 23:39:00 +0100

    further haskell learning

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -240,5 +240,66 @@ quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) =
   let smallerSorted = quicksort [a | a <- xs, a <= x]
-      biggerSorted   = quicksort [a | a <- xs, a > x]
+      biggerSorted  = quicksort [a | a <- xs, a > x]
   in smallerSorted ++ [x] ++ biggerSorted
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+  let smallerSorted = quicksort (filter (<=x) xs)
+      biggerSorted  = quicksort (filter (>x) xs)
+  in smallerSorted ++ [x] ++ biggerSorted
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred' :: (Num a, Ord a) => a -> Ordering
+compareWithHundred' = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _          = []
+zipWith' _ _ []          = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+  where g x y = f y x
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ []    = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x       = x : filter' p xs
+  | otherwise = filter' p xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n  = n : chain (3*n + 1) 
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
