42551ba93efb42cb3373bf87c7c82925da9a82a2
AnonyMouse-Box <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-04-14 15:21:10 +0100

    types and typeclasses

--- haskell/Geometry.hs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/runhaskell
-
-
-import qualified Geometry.Sphere as Sphere
-import qualified Geometry.Cuboid as Cuboid
-import qualified Geometry.Cube as Cube
--- haskell/baby.hs
+++ haskell/baby.hs
@@ -407,3 +407,14 @@ phoneBookToMap xs = Data.Map.fromListWith (\number1 number2 -> number1 ++ ", " +
 
 phoneBookToMap' :: (Ord k) => [(k, a)] -> Data.Map.Map k [a]
 phoneBookToMap' xs = Data.Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
+
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
