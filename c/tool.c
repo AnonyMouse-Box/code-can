a9fe0fe61b1a9ce8a02771f2f17a7e58d3755f95
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-07-24 08:49:30 +0100

    Add new file

---
+++ c/tool.c
@@ -0,0 +1,15 @@
+void linear_search(list[], item){}
+
+void binary_search(list[], item){}
+
+void selection_sort(list[]){}
+
+void insertion_sort(list[]){}
+
+void bubble_sort(list[]){}
+
+void merge_sort(list[]){}
+
+void quick_sort(list[]){}
+
+void heap_sort(list[]){}
