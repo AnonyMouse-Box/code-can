2198bf9bdcf80037375df3d8d1347f23de87b62c
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-07-24 13:29:53 +0100

    Update tools.c

--- c/tools.c
+++ c/tools.c
@@ -1,14 +1,8 @@
 #include <tools.h>
 
-int length(list[])
+int linear_search(int item, int list[], int length)
 {
-    sizeof *list / sizeof *list[0];
-}
-
-int linear_search(list[], int item)
-{
-    int len = length(list);
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
     {
         if (list[i] == item)
         {
@@ -18,10 +12,9 @@ int linear_search(list[], int item)
     return NULL;
 }
 
-int binary_search(list[], int item)
+int binary_search(int item, int list[], int length)
 {
-    int len = length(list);
-    if (len < 2)
+    if (length < 2)
     {
         if (list[0] == item)
         {
@@ -32,7 +25,7 @@ int binary_search(list[], int item)
             return NULL;
         }
     }
-    int centre = (len / 2) - 1;
+    int centre = (length / 2) - 1;
     int sublist[centre + 1];
     bool greater = false;
     if (list[centre] == item)
@@ -66,14 +59,14 @@ int binary_search(list[], int item)
     return index;
 }
 
-void selection_sort(list[]){}
+void selection_sort(int list[], int length){}
 
-void insertion_sort(list[]){}
+void insertion_sort(int list[], int length){}
 
-void bubble_sort(list[]){}
+void bubble_sort(int list[], int length){}
 
-void merge_sort(list[]){}
+void merge_sort(int list[], int length){}
 
-void quick_sort(list[]){}
+void quick_sort(int list[], int length){}
 
-void heap_sort(list[]){}
+void heap_sort(int list[], int length){}
