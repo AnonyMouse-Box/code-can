01cc50d3b0fba3dd4c6361433c5a63f2bd3bd74f
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-08-10 23:22:04 +0100

    Update tools.h

--- c/tools.h
+++ c/tools.h
@@ -1,10 +1,14 @@
 //I've tested and there is no in function way to calculate the length of an array, just run sizeof before running these functions
 //These functions are currently configured for lists of ints, reconfigure i/o as appropriate
-int linear_search(int item, int list[], int length);
-int binary_search(int item, int list[], int length);
-void selection_sort(int list[], int length);
-void insertion_sort(int list[], int length);
-void bubble_sort(int list[], int length);
-void merge_sort(int list[], int length);
-void quick_sort(int list[], int length);
-void heap_sort(int list[], int length);
+
+#include <stdbool.h>
+
+int linear_search(char item, char list[]);
+int binary_search(char item, char list[]);
+
+bool selection_sort(char list[], int length);
+bool insertion_sort(char list[], int length);
+bool bubble_sort(char list[], int length);
+bool merge_sort(char list[], int length);
+bool quick_sort(char list[], int length);
+bool heap_sort(char list[], int length);
