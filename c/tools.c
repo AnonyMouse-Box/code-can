d6fce66450537375a11b75acffb3e55c9a5160a6
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-08-10 23:55:53 +0100

    Update tools.c

--- c/tools.c
+++ c/tools.c
@@ -1,114 +1,57 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
 #include "tools.h"
 
-void swap(int *item_1, int *item_2)
+bool linear_search(const unsigned char item, const unsigned char list[], const unsigned int length, unsigned int *result)
 {
-    int store = *item_1;
-    *item_1 = *item_2;
-    *item_2 = store;
+    int index = 0;
+    for (; index < length; index++)
+    {
+        if (list[index] == item)
+        {
+            *result = index;
+            return true;
+        }
+    }
+    return false;
 }
 
-int linear_search(int item, int list[], int length)
+bool binary_search(const unsigned char item, const unsigned char list[], const unsigned int length, unsigned int *result)
 {
-    for (int i = 0; i < length; i++)
-    {
-        if (list[i] == item)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int binary_search(int item, int list[], int length)
-{
-    if (length < 2)
-    {
-        if (list[0] == item)
-        {
-            return 0;
-        }
-        else
-        {
-            return -1;
-        }
-    }
-    int centre = floor(length / 2);
-    int sublist[centre + 1];
-    bool greater = false;
-    if (list[centre] == item)
-    {
-        return centre;
-    }
-    else if (list[centre] < item)
-    {
-        for (int i = 0; i < centre; i++)
-        {
-            sublist[i] = list[i];
-        }
-    }
-    else
-    {
-        greater = true;
-        for (int j = centre + 1, k = 0; j < length; j++, k++)
-        {
-            sublist[k] = list[j];
-        }
-    }
-    int len = sizeof sublist / sizeof sublist[0];
-    int index = binary_search(item, sublist, len);
-    if (index == -1)
-    {
-        return -1;
-    }
-    else if (greater == true)
-    {
-        index += centre + 1;
-    }
+    int index = 0;
     return index;
 }
 
-void selection_sort(int list[], int length)
+bool selection_sort(unsigned char list[], const unsigned int length)
 {
-    if (length < 2)
-    {
-        return;
-    }
-    int select = 2147483647, position = 0;
-    for (int i = 0; i < length; i++)
-    {
-        if (list[i] < select)
-        {
-            select = list[i];
-            position = i;
-        }
-    }
-    if (position != 0)
-    {
-        swap(&list[0], &list[position]);
-    }
-    int sublist[length - 1];
-    for (int j = 1; j < length; j++)
-    {
-        sublist[j - 1] = list[j];
-    }
-    int len = sizeof sublist / sizeof sublist[0];
-    selection_sort(sublist, len);
-    for (int k = 1; k < length; k++)
-    {
-        list[k] = sublist[k - 1];
-    }
-    return;
+    bool result = true;
+    return result;
 }
 
-void insertion_sort(int list[], int length){}
+bool insertion_sort(unsigned char list[], const unsigned int length)
+{
+    bool result = true;
+    return result;
+}
 
-void bubble_sort(int list[], int length){}
+bool bubble_sort(unsigned char list[], const unsigned int length)
+{
+    bool result = true;
+    return result;
+}
 
-void merge_sort(int list[], int length){}
+bool merge_sort(unsigned char list[], const unsigned int length)
+{
+    bool result = true;
+    return result;
+}
 
-void quick_sort(int list[], int length){}
+bool quick_sort(unsigned char list[], const unsigned int length)
+{
+    bool result = true;
+    return result;
+}
 
-void heap_sort(int list[], int length){}
+bool heap_sort(unsigned char list[], const unsigned int length)
+{
+    bool result = true;
+    return result;
+}
