8f601f62f97d00906280e15e37a39d385b9a6ffc
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-08-12 09:15:09 +0100

    Update tools.h

--- c/tools.h
+++ c/tools.h
@@ -1,14 +1,15 @@
 //I've tested and there is no in function way to calculate the length of an array, just run sizeof before running these functions
 //These functions are currently configured for lists of chars, reconfigure i/o as appropriate
 
+#include <stdlib.h>
 #include <stdbool.h>
 
-bool linear_search(const unsigned char item, const unsigned char list[], const unsigned int length, unsigned int *result);
-bool binary_search(const unsigned char item, const unsigned char list[], const unsigned int length, unsigned int *result);
+bool linear_search(const char item, const char list[], const int length, int *result);
+bool binary_search(const char item, const char list[], const int length, int *result);
 
-bool selection_sort(unsigned char list[], const unsigned int length);
-bool insertion_sort(unsigned char list[], const unsigned int length);
-bool bubble_sort(unsigned char list[], const unsigned int length);
-bool merge_sort(unsigned char list[], const unsigned int length);
-bool quick_sort(unsigned char list[], const unsigned int length);
-bool heap_sort(unsigned char list[], const unsigned int length);
+bool selection_sort(char list[], const int length);
+bool insertion_sort(char list[], const int length);
+bool bubble_sort(char list[], const int length);
+bool merge_sort(char list[], const int length);
+bool quick_sort(char list[], const int length);
+bool heap_sort(char list[], const int length);
