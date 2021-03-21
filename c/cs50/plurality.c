9b7bbb1d9b222d9ea6dc3e16aab5fc036e72bdfc
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2021-03-21 14:04:35 +0000

    Upload New File

---
+++ c/cs50/plurality.c
@@ -0,0 +1,102 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // TODO
+    for (int i = 0; i < candidate_count; i++) //search candidate list for name
+    {
+        if (strcmp(name, candidates[i].name) == 0) //if match increment relevant votes and exit
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    int votes_max = 0;
+    for (int i = 0; i < candidate_count; i++) //iterate through all candidates, takes the max
+    {
+        if (candidates[i].votes > votes_max)
+        {
+            votes_max = candidates[i].votes;
+        }
+    }
+    for (int i = 0; i < candidate_count; i++) //print all candidate names that match max
+    {
+        if (votes_max == candidates[i].votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    return;
+}
+
